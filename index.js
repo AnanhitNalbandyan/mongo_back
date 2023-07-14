8dcc1da51fd467a0908568df7621932448beda48
Hayk <hayk.inanc@gmail.com>
2023-07-14 15:04:41 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const users = require('./routes/users')
+
+const PORT = 3333;
+const app = express();
+app.use(express.json());
+app.use('/users', users);
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT} port`);
+})
+
+
